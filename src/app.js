6dedbcb60beffb37b3a656831139fae7045337a6
hector <hpulido1021@gmail.com>
2023-11-19 21:52:00 -0500

    Feature: created get request on app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
+app.get("/restaurants", async (req, res) => {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+    
+})
 
 
 
