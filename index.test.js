42d872fd08781df03d9e18b31dd21d4e85665766
hector <hpulido1021@gmail.com>
2023-11-25 21:24:05 -0500

    Feature: Created index.test.js with unit test of restaurant model

---
+++ index.test.js
@@ -0,0 +1,62 @@
+const request = require("supertest");
+const app = require("./src/app")
+const Restaurant = require("./models/index")
+const syncSeed = require("./seed")
+let restQuantity;
+
+beforeAll(async() => {
+    await syncSeed ()
+    const restaurants = await Restaurant.findAll({})
+    restQuantity = restaurants.length;
+})
+ describe("Check restaurant routes", () => {
+    test("should return 200 statuscode", async () => {
+      const response = await request(app).get("/restaurants");
+      expect(response.statusCode).toBe(200);
+    })
+    test("should return an array", async () => {
+        const response = await request(app).get("/restaurants");
+        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.body[0]).toHaveProperty("cuisine")
+      })
+      test("should return correct length", async () => {
+        const response = await request(app).get("/restaurants");
+        expect(response.body.length).toBe(restQuantity);
+      })
+      test("should return the correct data", async () => {
+        const response = await request(app).get("/restaurants");
+        expect(response.body).toContainEqual(
+            expect.objectContaining({
+                id: 1,
+                name: "AppleBees",
+                location: "Texas",
+                cuisine: "FastFood",
+            })
+        );
+      })
+      test("should return the correct data", async () => {
+        const response = await request(app).get("/restaurants/1");
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                id: 1,
+                name: "AppleBees",
+                location: "Texas",
+                cuisine: "FastFood",
+            })
+        );
+      })
+      test("should return with a new value", async () => {
+        const response = await request(app).post("/restaurants").send({name:"asd", location: "wsa", cuisine: "jskd"})
+        expect(response.body.length).toEqual(restQuantity + 1 );
+      })
+      test("should return with an apdated value", async () => {
+        await request(app).put("/restaurants/1").send({name:"adf", location: "wsa", cuisine: "jskd"})
+        const restaurant = await  Restaurant.findByPk(1)
+        expect(restaurant.name).toEqual("adf");
+      })
+      test("should delete db entry id", async () => {
+        await request(app).delete("/restaurants/1")
+        const restaurant = await  Restaurant.findAll({})
+        expect(restaurant.length).toEqual(restQuantity);
+      })
+ })
--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 app.get("/restaurants", async (req, res) => {
     const restaurants = await Restaurant.findAll();
     res.json(restaurants);  
@@ -13,6 +16,19 @@ app.get("/restaurants/:id", async (req,res) => {
     res.json(id);
 })
 
+app.post("/restaurants", async (req, res) => {
+    const newRestaurant = await Restaurant.create(req.body)
+    const newRestaurants = await Restaurant.findAll({})
+    res.json(newRestaurants);
+})
 
+app.put("/restaurants/:id", async (req, res) => {
+const updatedRestaurant = await Restaurant.update(req.body, {where: {id: req.params.id}});
+res.json(updatedRestaurant);
+})
+app.delete("/restaurants/:id", async (req,res) => {
+    const destroyRestaurant = await Restaurant.destroy({ where: {id: req.params.id}})
+    res.json(destroyRestaurant);
+})
 
 module.exports = app;
